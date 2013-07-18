a0994721c35ed340591001d02480baa9ac7455d8
Vinicius@dell <vinicius.darocha@das.ufsc.br>
2013-07-18 00:19:01 -0300

    first profile.clj version

---
+++ profiles.clj
@@ -0,0 +1,7 @@
+{:user {:plugins [[lein-difftest "1.3.8"]
+                  [lein-marginalia "0.7.1"]
+                  [lein-pprint "1.1.1"]
+                  [lein-swank "1.4.5"]
+                  [lein-localrepo "0.5.0"]
+                  [org.clojars.kurtharriger/lein-depgraph "0.1.0"]
+                  [lein-ring "0.8.6"]]}}
